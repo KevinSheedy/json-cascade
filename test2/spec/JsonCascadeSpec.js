a9917e382b4fb8f1cba1c89258b742d1542ad00c
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2014-12-17 11:46:29 +0000

    Jasmine 2.1.3

---
+++ test2/spec/JsonCascadeSpec.js
@@ -0,0 +1,12 @@
+if( typeof jsonCascade === 'undefined' ) {
+  var jsonCascade = require('../../json-cascade');
+}
+
+describe("Player", function() {
+
+  it("should be able to play a Song", function() {
+    
+    expect(jsonCascade()).toEqual("");
+
+  });
+});
