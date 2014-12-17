3f069d13beebb6fbbe3122f86b49e310e9e5a958
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2014-12-17 17:17:32 +0000

    Adding unit tests

--- json-cascade.js
+++ json-cascade.js
@@ -1,11 +1,19 @@
 "use strict";
 
 (function() {
+
 	var root = this
 	var previousJsonCascade = root.jsonCascade;
 
 	var jsonCascade = function() {
-		return "";
+
+		if(arguments.length == 0)
+			return {};
+
+		var template = arguments[0];
+
+		// Can't use slice on arguments
+		var models = subsetOfArguments(1, arguments);
 	}
 
 	jsonCascade.noConflict = function() {
@@ -32,6 +40,20 @@
 			_ = require('underscore')
 		}
 		else throw new Error('mymodule requires underscore, see http://underscorejs.org');
+	}
+
+
+
+	function isArrayTemplate(val) {
+		return is.array(val) && val.length == 1 && is.object(val[0]);
+	}
+
+	function subsetOfArguments(index, arguments) {
+		var arr = [];
+		for(var i = index; i < arguments.length; i++) {
+			arr.push(arguments[i]);
+		}
+		return out;
 	}
 
 }).call(this);
--- test/json-cascade.spec.js
+++ test/json-cascade.spec.js
@@ -2,10 +2,36 @@ if( typeof jsonCascade === 'undefined' ) {
 	var jsonCascade = require('../json-cascade');
 }
 
-describe('jsonCascade', function(){
+describe('jsonCascade simple objects', function(){
 
-	it('something must be done', function(){
-		expect( jsonCascade() ).toBe( "" );
+	var actual, expected, template, lowPrecedence, midPrecedence, highPrecedence;
+
+
+	it('should return an empty object', function(){
+		expect( typeof jsonCascade() ).toBe( 'object' );
 	})
+
+
+	it('should return just the template if the template is the only param', function() {
+		template = {foo: 'bar'};
+		actual = jsonCascade(template);
+		expect( actual.foo ).toBe( 'bar' );
+	})
+
+	it('should override the template', function() {
+		template = {foo: 'bar'};
+		lowPrecedence = {foo: 'lowOverride'};
+		actual = jsonCascade(template, midPrecedence);
+		expect( actual.foo ).toBe( 'lowOverride' );
+	})
+
+	it('should override the template with the highest precedence diff', function() {
+		template = {foo: 'bar'};
+		lowPrecedence = {foo: 'lowOverride'};
+		midPrecedence = {foo: 'midOverride'};
+		actual = jsonCascade(template, lowPrecedence, midPrecedence);
+		expect( actual.foo ).toBe( 'midOverride' );
+	})
+
 
 })
